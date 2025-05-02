775813c97ac0a72a6b7b61f73e136f6c081973f8
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-05-02 03:07:30 -0700

    Updated controller to include logic to switch scene to Dashboard

--- src/main/java/com/example/automatingtextprocessing/HelloController.java
+++ src/main/java/com/example/automatingtextprocessing/HelloController.java
@@ -1,7 +1,15 @@
 package com.example.automatingtextprocessing;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -11,4 +19,29 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void handleGoToDashboard(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Text Processing System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleExit(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText(null);
+        alert.setContentText("Goodbye!");
+        alert.showAndWait();
+
+        // Close the window
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 }
